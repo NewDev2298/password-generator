48340f773e0de538f089de4981ce18129d1d5b00
Terrance Smith <terrancesmith@Laquishas-Air.lan>
2022-11-30 17:51:40 -0500

    Added confirmation declarations within function.

--- script.js
+++ script.js
@@ -3,19 +3,35 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // TODO: CODE GOES HERE
-  return '';
-
+  
 
   var isUppercase = confirm('Would you like to use uppercase?');
-  var isLowercase = false;
-  var isSpecial = false;
-  var isNumber = false;
+  var isLowercase = confirm('Would you like to use lowercase?');
+  var isSpecial = confirm('Would you like to use special characters?');
+  var isNumber = confirm('Would you like to you numbers?');
 
   if (isUppercase) {
     // USE UPPERCASE LETTERS
     console.log('uppercase');
-
   }
+
+  if (isLowercase) {
+    // USE LOWERCASE LETTERS
+    console.log('lowercase');
+  }
+
+  if (isSpecial) {
+    // USE SPECIAL LETTERS
+    console.log('special');
+  }
+
+  if (isNumber) {
+    // USE NUMBERS
+    console.log('number');
+  }
+
+  return '';
+
 }
 
 
