f1b0669ca30d133c30ec973ac0e4acbc14c45d18
Terrance Smith <terrancesmith@Laquishas-Air.lan>
2022-11-30 17:16:52 -0500

    replaced created files with starter code

---
+++ script.js
@@ -0,0 +1,29 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  // TODO: CODE GOES HERE
+  var isUppercase = confirm('Would you like to use uppercase?');
+  var isLowercase = false;
+  var isSpecial = false;
+  var isNumber = false;
+
+  if (isUppercase) {
+    // USE UPPERCASE LETTERS
+    console.log('uppercase');
+
+  }
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
